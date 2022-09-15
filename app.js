cd22565e7f7d92a33cc0473d6791fa15998454bb
Eddie Z <e.zarate7@hotmail.com>
2022-09-15 08:10:11 -0600

    update\

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-console.log("Hello World!\n==========\n");
-console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
-);
+
+  
+
 
 // Exercise 1
-
+const firstName = "Eddie";
+let lastName = "Zarate";
+var age = 32;
 // Exercise 2
-
+lastName = "Zarate";
+age = 21;
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`Hello! My name is ${firstName} ${lastName}! I am ${age}. I am pretending \n
+I know ${language} in case you want to send me a shirt my size is ${selectedSize}`  );
